20703860491eb38ac21b159e680811e7af689f4f
raventeee <69177198+raventeee@users.noreply.github.com>
2022-06-16 00:56:57 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+// require('dotenv').config();
 const fs = require ("fs");
 const { REST } = require("@discordjs/rest");
 const { Routes } = require("discord-api-types/v9");
